ea68c03501f024e441e4cfae949e73c344919a37
John Else <john.else@citrix.com>
2012-04-02 00:48:48 +0100

    Initial commit.
    Signed-off-by: John Else <john.else@citrix.com>

---
+++ config.py
@@ -0,0 +1,44 @@
+# 
+# Copyright (c) 2006-2012 XenSource, Inc. All use and distribution of this
+# copyrighted material is governed by and subject to terms and
+# conditions as licensed by XenSource, Inc. All other rights reserved.
+#
+
+import getpass
+import os
+
+config_file = ".defects.rc"
+
+jira_url       = None
+confluence_url = None
+username       = None
+password       = None
+
+def load_config():
+    global jira_url, confluence_url, username, password
+    if None in [jira_url, confluence_url, username, password]:
+        user = getpass.getuser()
+        homedir = os.path.expanduser("~" + user)
+        filename = os.path.join(homedir, config_file)
+        try:
+            f = open(filename)
+            try:
+                for line in f:
+                    pair = line.split("=")
+                    if len(pair) == 2:
+                        key  = pair[0]
+                        value = pair[1][0:-1]
+                        if (jira_url is None) and (key == "jira_url"):
+                            jira_url = value
+                        elif (confluence_url is None) and (key == "confluence_url"):
+                            confluence_url = value
+                        elif (username is None) and (key == "username"):
+                            username = value
+                        elif (password is None) and (key == "password"):
+                            password = value
+            finally:
+                f.close()
+        except:
+            sys.stderr.write("Could not access the configuration file.\n")
+            sys.exit(3)
+
