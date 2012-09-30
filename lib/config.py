89d8f9e282461abaaea91d10102fa6628f45fcf8
John Else <john.else@citrix.com>
2012-10-01 00:27:22 +0100

    Simplified the config loading system.
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/config.py
+++ lib/config.py
@@ -5,52 +5,34 @@
 #
 
 import getpass
+import imp
 import os
 import sys
 
-config_file = ".defects.rc"
+config_file = ".defecttools/config.py"
 
-jira_url            = None
-jira_username       = None
-jira_password       = None
-confluence_url      = None
-confluence_username = None
-confluence_password = None
-database            = None
+expected_keys = [
+    "jira_url",
+    "jira_username",
+    "jira_password",
+    "confluence_url",
+    "confluence_username",
+    "confluence_password",
+    "database"
+    ]
 
-def load_config():
-    global jira_url, jira_username, jira_password
-    global confluence_url, confluence_username, confluence_password
-    global database
-    if None in [jira_url, jira_username, jira_password, confluence_url, confluence_username, confluence_password, database]:
-        user = getpass.getuser()
-        homedir = os.path.expanduser("~" + user)
-        filename = os.path.join(homedir, config_file)
-        try:
-            f = open(filename)
-            try:
-                for line in f:
-                    pair = line.split("=", 1)
-                    if len(pair) == 2:
-                        key  = pair[0]
-                        value = pair[1][0:-1]
-                        if (jira_url is None) and (key == "jira_url"):
-                            jira_url = value
-                        if (jira_username is None) and (key == "jira_username"):
-                            jira_username = value
-                        if (jira_password is None) and (key == "jira_password"):
-                            jira_password = value
-                        elif (confluence_url is None) and (key == "confluence_url"):
-                            confluence_url = value
-                        elif (confluence_username is None) and (key == "confluence_username"):
-                            confluence_username = value
-                        elif (confluence_password is None) and (key == "confluence_password"):
-                            confluence_password = value
-                        elif (database is None) and (key == "database"):
-                            database = value
-            finally:
-                f.close()
-        except:
-            sys.stderr.write("Could not access the configuration file.\n")
-            sys.exit(3)
-
+def get_config():
+    user = getpass.getuser()
+    home_dir = os.path.expanduser("~" + user)
+    config_path = os.path.join(home_dir, config_file)
+    try:
+        config = imp.load_source("config", config_path)
+        for key in expected_keys:
+            config.__getattribute__(key)
+        return config
+    except IOError:
+        raise RuntimeError("No config file found at %s." % config_path)
+    except SyntaxError:
+        raise RuntimeError("Could not parse config file.")
+    except AttributeError as e:
+        raise RuntimeError("Missing config key: %s" % e.args[0])
