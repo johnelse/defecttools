e820514937478fcd35c0473ee3d226ee399868b4
Harrie Pieters <harriepieters@gmail.com>
2013-04-17 18:55:08 +0100

    Clean up, License, update README

--- cronjob/cronjob.sh
+++ cronjob/cronjob.sh
@@ -1,24 +1,40 @@
-STDOUT_FILE=/home/ring3defects/logs/ddd_out
-STDERR_FILE=/home/ring3defects/logs/ddd_err
+# 
+# Copyright (C) Citrix Systems Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; version 2.1 only. with the special
+# exception on linking described in file LICENSE.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
 
+# Configuration
+
+# Log location
+STDOUT_FILE=/home/ring3defects/logs/ddd_out
+# Errorlog location
+STDERR_FILE=/home/ring3defects/logs/ddd_err
+# Database location
+DATA_DIR=/home/ring3defects/data/defect_dashboard
+# Webserver directory
+#WEB_DIR=/home/ring3defects/public_html
+# Current data
+DATE=`date +"%d%m%Y"`
+
+# Make a log entry start
 printf "===$0 Started at: " | tee -a $STDOUT_FILE $STDERR_FILE
 date | tee -a $STDOUT_FILE  $STDERR_FILE
 
-
+#Get data from jira and insert in the database
 /home/ring3defects/defecttools/scripts/retrieve_data >> $STDOUT_FILE 2>> $STDERR_FILE
+#Get data from database, compare, calculate, and update wiki
 /home/ring3defects/defecttools/scripts/update_graphs >> $STDOUT_FILE 2>> $STDERR_FILE
-
-DATA_DIR=/home/ring3defects/data/defect_dashboard
-#WEB_DIR=/home/ring3defects/public_html
-DATE=`date +"%d%m%Y"`
-
 # Snapshot the database.
 cp $DATA_DIR/tickets.db $DATA_DIR/backups/tickets-${DATE}.db
-# Copy the database to the directory being served over HTTP.
-#cp -f $DATA_DIR/tickets.db $WEB_DIR/db
-# Copy the new snapshot to the NFS mount.
-#rsync $DATA_DIR/backups/* /mnt/ermintrude/backups
 
+# Finish log entry end
 printf "===$0 Ended at: " | tee -a $STDOUT_FILE $STDERR_FILE
 date | tee -a $STDOUT_FILE  $STDERR_FILE
-
